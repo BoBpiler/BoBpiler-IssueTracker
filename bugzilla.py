90cf3d7639d6b8e3d9aaf3312624994686efdb77
Gyumin Baek <guminb@ajou.ac.kr>
2023-10-26 20:03:54 +0900

    chunk size added

--- bugzilla.py
+++ bugzilla.py
@@ -6,6 +6,61 @@ from concurrent.futures import ThreadPoolExecutor, wait
 # 검색 키워드 리스트 - 추가 및 삭제가 가능합니다.
 keywords = ["optimization", "miscompilation"]
 
+# unique_bugs를 chunk_size만큼 나눠서 여러 파일에 저장하는 함수 (탐지된 버그 개수가 너무 많은 경우를 위해서 1000개 단위로 끊었음)
+def save_to_files(base_url, compiler_name, formatted_time, unique_bugs, chunk_size=1000):
+    
+    num_chunks = len(unique_bugs) // chunk_size + (1 if len(unique_bugs) % chunk_size else 0)
+    
+    for chunk_idx in range(num_chunks):
+        start_idx = chunk_idx * chunk_size
+        end_idx = start_idx + chunk_size
+        current_chunk = list(unique_bugs)[start_idx:end_idx]
+
+        # chunk 별로 파일 이름 수정
+        md_filename = f"{compiler_name}_bugzilla_{formatted_time}_chunk_{chunk_idx + 1}.md"
+        json_filename = f"{compiler_name}_bugzilla_{formatted_time}_chunk_{chunk_idx + 1}.json"
+
+        # 각 chunk에 대한 데이터 저장 로직 
+        with open(md_filename, "w", encoding="utf-8") as md_file, open(json_filename, "w", encoding="utf-8") as json_file:
+            details_list = []
+            for bug in current_chunk:
+                comment_url = f"{base_url}/{bug['id']}/comment"
+                comment_response = requests.get(comment_url)
+                comments_data = comment_response.json().get("bugs", {}).get(str(bug['id']), {}).get("comments", [])
+                description = comments_data[0]['text'] if comments_data else "No comments available."           # 첫 번째 코멘트를 본문 내용으로 사용
+                
+                # 상세 정보를 딕셔너리로 조합
+                details = {
+                    "compiler": f"{compiler_name}",
+                    "title": bug['summary'],
+                    "open_at": bug['creation_time'],
+                    "last_modified_date": bug['last_change_time'],
+                    "link": f"{base_url.replace('rest.cgi/bug', 'show_bug.cgi')}?id={bug['id']}",
+                    "status": bug['status'],
+                    "tags": bug['keywords'],
+                    "component": bug['component'],
+                    "version": bug['version'],
+                    "severity": bug['severity'],
+                    "contents": description,
+                }
+                
+                details_list.append(details)
+
+                md_file.write("\n\n")  # 구분선
+                md_file.write(f"### compiler : `{compiler_name}`\n")
+                md_file.write(f"### title : `{details['title']}`\n")
+                md_file.write(f"### open_at : `{details['open_at']}`\n")
+                md_file.write(f"### last_modified_date : `{details['last_modified_date']}`\n")
+                md_file.write(f"### link : {details['link']}\n")
+                md_file.write(f"### status : `{details['status']}`\n")
+                md_file.write(f"### tags : `{', '.join(details['tags'])}`\n")
+                md_file.write(f"### component : `{details['component']}`\n")
+                md_file.write(f"### version : `{details['version']}`\n")
+                md_file.write(f"### severity : `{details['severity']}`\n")
+                md_file.write(f"### contents :\n{description}\n\n\n")
+                md_file.write("---\n")  # 구분선
+            json.dump(details_list, json_file, ensure_ascii=False, indent=4)
+
 # 버그질라에서 버그 리포팅을 수집하는 함수
 def scraping_bugs_from_bugzilla(base_url, compiler_name, search_keywords):
     # 현재 날짜에서 5년 전의 날짜 계산
@@ -31,50 +86,9 @@ def scraping_bugs_from_bugzilla(base_url, compiler_name, search_keywords):
 
     # 버그 id를 기반으로 중복 제거
     unique_bugs = {bug['id']: bug for bug in bugs}.values()
-
-    # 탐지된 모든 버그의 상세 정보를 저장하는 리스트
-    details_list = []
-    # 결과를 readable하게 가공해서 md 파일로 저장
-    with open(f"{compiler_name}_bugzilla_{formatted_time}.md", "w", encoding="utf-8") as md_file:
-        for bug in unique_bugs:
-            comment_url = f"{base_url}/{bug['id']}/comment"
-            comment_response = requests.get(comment_url)
-            comments_data = comment_response.json().get("bugs", {}).get(str(bug['id']), {}).get("comments", [])
-            description = comments_data[0]['text'] if comments_data else "No comments available."           # 첫 번째 코멘트를 본문 내용으로 사용
-            
-            # 상세 정보를 딕셔너리로 조합
-            details = {
-                "compiler": f"{compiler_name}",
-                "title": bug['summary'],
-                "open_at": bug['creation_time'],
-                "last_modified_date": bug['last_change_time'],
-                "link": f"{base_url.replace('rest.cgi/bug', 'show_bug.cgi')}?id={bug['id']}",
-                "status": bug['status'],
-                "tags": bug['keywords'],
-                "component": bug['component'],
-                "version": bug['version'],
-                "severity": bug['severity'],
-                "contents": description,
-            }
-            
-            details_list.append(details)
-
-            md_file.write("\n\n")  # 구분선
-            md_file.write(f"### compiler : `{compiler_name}`\n")
-            md_file.write(f"### title : `{details['title']}`\n")
-            md_file.write(f"### open_at : `{details['open_at']}`\n")
-            md_file.write(f"### last_modified_date : `{details['last_modified_date']}`\n")
-            md_file.write(f"### link : {details['link']}\n")
-            md_file.write(f"### status : `{details['status']}`\n")
-            md_file.write(f"### tags : `{', '.join(details['tags'])}`\n")
-            md_file.write(f"### component : `{details['component']}`\n")
-            md_file.write(f"### version : `{details['version']}`\n")
-            md_file.write(f"### severity : `{details['severity']}`\n")
-            md_file.write(f"### contents :\n{description}\n\n\n")
-            md_file.write("---\n")  # 구분선
-
-    with open(f"{compiler_name}_bugzilla_{formatted_time}.json", "w", encoding="utf-8") as json_file:
-        json.dump(details_list, json_file, ensure_ascii=False, indent=4)
+    
+    # unique_bugs를 파일로 저장, chunk size 기준으로 md, json 파일 생성
+    save_to_files(base_url, compiler_name, formatted_time, unique_bugs)
 
 
 if __name__ == "__main__":
