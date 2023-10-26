a214556b320e92d9807aad87280a62ac5567cd96
Gyumin Baek <guminb@ajou.ac.kr>
2023-10-26 17:18:01 +0900

    modified

--- gcc.py
+++ gcc.py
@@ -1,9 +1,13 @@
 import requests
 import datetime
+import json
 
 # 현재 날짜에서 5년 전의 날짜 계산
 end_date = datetime.date.today()
 start_date = end_date - datetime.timedelta(days=5*365)  # 대략적인 5년 전
+
+# 검색 키워드 리스트 - 추가 및 제거로 조절이 가능합니다.
+search_keywords = ["optimization", "miscompilation"]
 
 # GCC의 버그질라 API 엔드포인트 및 키워드 설정
 base_url = "https://gcc.gnu.org/bugzilla/rest.cgi/bug"
@@ -14,48 +18,54 @@ params = {
     "include_fields": "id,summary,status,resolution,component,version,severity,creation_time,last_change_time,keywords",
 }
 
+# 스크래핑 시작
 response = requests.get(base_url, params=params)
-
-# 결과 확인 및 출력
 bugs = response.json().get("bugs", [])
-for bug in bugs:
-    print("*"*100) # 구분선
-    # 각 버그의 코멘트를 가져오는 API 호출
-    comment_url = f"{base_url}/{bug['id']}/comment"
-    comment_response = requests.get(comment_url)
-    comments_data = comment_response.json().get("bugs", {}).get(str(bug['id']), {}).get("comments", [])
-    description = comments_data[0]['text'] if comments_data else "No comments available."  # 첫 번째 코멘트를 본문 내용으로 사용
-    # 상세 정보를 딕셔너리로 조합
-    details = {
-        "link": f"https://gcc.gnu.org/bugzilla/show_bug.cgi?id={bug['id']}",
-        "title": bug['summary'],
-        "status": bug['status'],
-        "component": bug['component'],
-        "version": bug['version'],
-        "severity": bug['severity'],
-        "reported_date": bug['creation_time'],
-        "modified_date": bug['last_change_time'],
-        "keywords": bug['keywords'],
-        "contents": description,
-    }
-    link = f"link: {details['link']}"
-    title = f"title: {details['title']}"
-    status = f"status: {details['status']}"
-    component = f"component: {details['component']}"
-    version = f"version: {details['version']}"
-    severity = f"severity: {details['severity']}"
-    reported_date = f"reported_date: {details['reported_date']}" 
-    modified_date = f"modified_date: {details['modified_date']}" 
-    keywords = f"keywords: {details['keywords']}"
-    contents = f"contents: {description}"
-    print(link)
-    print(title)
-    print(status)
-    print(component)
-    print(version)
-    print(severity)
-    print(reported_date)
-    print(modified_date)
-    print(keywords)
-    print(contents)
 
+# 탐지된 모든 버그의 상세 정보를 저장하는 리스트
+details_list = []
+
+# 결과를 readable하게 가공해서 md 파일로 저장
+with open("gcc.md", "w", encoding="utf-8") as md_file:
+
+    for bug in bugs:
+        # 각 버그의 코멘트를 가져오는 API 호출
+        comment_url = f"{base_url}/{bug['id']}/comment"
+        comment_response = requests.get(comment_url)
+        comments_data = comment_response.json().get("bugs", {}).get(str(bug['id']), {}).get("comments", [])
+        description = comments_data[0]['text'] if comments_data else "No comments available."  # 첫 번째 코멘트를 본문 내용으로 사용
+        
+        # 상세 정보를 딕셔너리로 조합
+        details = {
+            "compiler": "gcc",
+            "title": bug['summary'],
+            "open_at": bug['creation_time'],
+            "last_modified_date": bug['last_change_time'],
+            "link": f"https://gcc.gnu.org/bugzilla/show_bug.cgi?id={bug['id']}",
+            "status": bug['status'],
+            "tags": bug['keywords'],
+            "component": bug['component'],
+            "version": bug['version'],
+            "severity": bug['severity'],
+            "contents": description,
+        }
+        
+        details_list.append(details)
+
+        md_file.write("\n\n")  # 구분선
+        md_file.write(f"### compiler : `gcc`\n")
+        md_file.write(f"### title : `{details['title']}`\n")
+        md_file.write(f"### open_at : `{details['open_at']}`\n")
+        md_file.write(f"### last_modified_date : `{details['last_modified_date']}`\n")
+        md_file.write(f"### link : {details['link']}\n")
+        md_file.write(f"### status : `{details['status']}`\n")
+        md_file.write(f"### tags : `{', '.join(details['tags'])}`\n")
+        md_file.write(f"### component : `{details['component']}`\n")
+        md_file.write(f"### version : `{details['version']}`\n")
+        md_file.write(f"### severity : `{details['severity']}`\n")
+        md_file.write(f"### contents :\n{description}\n\n")
+        md_file.write("---")  # 구분선
+
+# details_list를 JSON 파일로 저장
+with open("details.json", "w", encoding="utf-8") as json_file:
+    json.dump(details_list, json_file, ensure_ascii=False, indent=4)
