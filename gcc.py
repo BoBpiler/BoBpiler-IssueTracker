790319dfb0305f6007266be303860f240cb2941a
Gyumin Baek <guminb@ajou.ac.kr>
2023-10-26 11:21:54 +0900

    gcc scraping test code

--- gcc.py
+++ gcc.py
@@ -5,13 +5,14 @@ import datetime
 end_date = datetime.date.today()
 start_date = end_date - datetime.timedelta(days=5*365)  # 대략적인 5년 전
 
-# 버그질라 API 엔드포인트 및 키워드 설정
-base_url = "https://bugzilla.mozilla.org/rest/bug"
+# GCC의 버그질라 API 엔드포인트 및 키워드 설정
+base_url = "https://gcc.gnu.org/bugzilla/rest.cgi/bug"
 params = {
-    "keywords": "optimization",  # 예시 키워드
-    "chfieldfrom": start_date,  # 변경 시작 날짜
-    "chfieldto": end_date,      # 변경 끝 날짜
-    "include_fields": "id,summary,status,resolution"  # 원하는 필드만 포함
+    "keywords": "optimization", 
+    "chfieldfrom": start_date,  
+    "chfieldto": end_date,      
+    "include_fields": "id,summary,status,resolution", 
+    "quicksearch": "gcc | clang"  # 이 부분은 필요에 따라 조정이 가능합니다.
 }
 
 response = requests.get(base_url, params=params)
