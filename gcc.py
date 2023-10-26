653f36f0f0c8d54f3c4ed25b498d14ac557b2ddc
Gyumin Baek <guminb@ajou.ac.kr>
2023-10-26 17:55:28 +0900

    modified

--- gcc.py
+++ gcc.py
@@ -10,27 +10,33 @@ start_date = end_date - datetime.timedelta(days=5*365)  # 대략적인 5년 전
 search_keywords = ["optimization", "miscompilation"]
 
 # GCC의 버그질라 API 엔드포인트 및 키워드 설정
-base_url = "https://gcc.gnu.org/bugzilla/rest.cgi/bug"
+gcc_base_url = "https://gcc.gnu.org/bugzilla/rest.cgi/bug"
 params = {
-    "keywords": "optimization", 
+    "keywords": "", 
     "chfieldfrom": start_date,  
     "chfieldto": end_date,  
     "include_fields": "id,summary,status,resolution,component,version,severity,creation_time,last_change_time,keywords",
 }
 
-# 스크래핑 시작
-response = requests.get(base_url, params=params)
-bugs = response.json().get("bugs", [])
+# 모든 키워드에 대한 버그 검색 결과를 담는 리스트
+bugs = []
+# 한 번의 검색에 키워드 한 개만 가능해서 for문을 통해서 여러 번 검색
+for search_keyword in search_keywords:
+    params['keywords'] = search_keyword
+    response = requests.get(gcc_base_url, params=params)
+    bugs.extend(response.json().get("bugs", []))
+
+# 버그 id를 기반으로 중복 제거
+unique_bugs = {bug['id']: bug for bug in bugs}.values()  # 중복 제거
 
 # 탐지된 모든 버그의 상세 정보를 저장하는 리스트
 details_list = []
-
 # 결과를 readable하게 가공해서 md 파일로 저장
 with open("gcc.md", "w", encoding="utf-8") as md_file:
 
-    for bug in bugs:
+    for bug in unique_bugs:
         # 각 버그의 코멘트를 가져오는 API 호출
-        comment_url = f"{base_url}/{bug['id']}/comment"
+        comment_url = f"{gcc_base_url}/{bug['id']}/comment"
         comment_response = requests.get(comment_url)
         comments_data = comment_response.json().get("bugs", {}).get(str(bug['id']), {}).get("comments", [])
         description = comments_data[0]['text'] if comments_data else "No comments available."  # 첫 번째 코멘트를 본문 내용으로 사용
@@ -63,9 +69,9 @@ with open("gcc.md", "w", encoding="utf-8") as md_file:
         md_file.write(f"### component : `{details['component']}`\n")
         md_file.write(f"### version : `{details['version']}`\n")
         md_file.write(f"### severity : `{details['severity']}`\n")
-        md_file.write(f"### contents :\n{description}\n\n")
-        md_file.write("---")  # 구분선
+        md_file.write(f"### contents :\n{description}\n\n\n")
+        md_file.write("---\n")  # 구분선
 
 # details_list를 JSON 파일로 저장
-with open("details.json", "w", encoding="utf-8") as json_file:
+with open("gcc.json", "w", encoding="utf-8") as json_file:
     json.dump(details_list, json_file, ensure_ascii=False, indent=4)
